32b3f1f6e7a2708c6657b840ac5dbd26ad247217
aguestuser <guest.austin@gmail.com>
2015-07-30 17:01:25 -0400

    Add mock click and press handlers to goButton

--- src/components/GoButton.jsx
+++ src/components/GoButton.jsx
@@ -1,23 +1,45 @@
 const Marty = require('marty');
 const BaseComponent = require('./BaseComponent');
+const Tappable = require('react-tappable');
 
-const go = {
-  radius: 110,
-  color: {
-    off: '#dd3b47',
-    on: '#30ff40'
-  }
-};
 
 class GoButton extends BaseComponent {
-  render(){
-    return (
-          <svg className="goButton" width={go.radius*2} height={go.radius*2}>
-            <circle cx={go.radius} cy={go.radius} r={go.radius} fill={go.color.off} />
-          </svg>
 
+  constructor(){
+    super();
+    this.go = {
+      radius: 110,
+      diameter: () => this.go.radius * 2,
+      color: {
+        off: '#dd3b47',
+        on: '#30ff40'
+      }
+    };
+    this.events = {
+      onClick: this._handleClick,
+      onPress: this._handlePress
+    };
+  }
+
+  render(){
+    const { go, events } = this;
+    return (
+      <Tappable {...events} >
+        <svg className="goButton" width={go.diameter()} height={go.diameter()}>
+          <circle cx={go.radius} cy={go.radius} r={go.radius} fill={go.color.off} />
+        </svg>
+      </Tappable>
     );
   };
+
+  _handleClick(){
+    alert('I got clicked!');
+  }
+
+  _handlePress(){
+    alert('I got pressed!');
+  }
+
 }
 
 module.exports = Marty.createContainer(GoButton);
