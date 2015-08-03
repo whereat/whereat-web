b6509e5c0221f7ad1024f65c2aa6dd6047e103ce
imipolex-g <imipolex-g@riseup.net>
2015-08-03 19:47:27 -0400

    Add NavStore * #goto handler * #getPage accessor * passing specs

--- src/stores/NavStore.js
+++ src/stores/NavStore.js
@@ -1,43 +1,34 @@
 const Marty = require('marty');
 const NavConstants = require('../constants/NavConstants');
+const { HOME, MAP } = require('../constants/Pages');
 const { Map } = require('immutable');
 
-class GoButtonStore extends Marty.Store {
+class NavStore extends Marty.Store {
 
   constructor(options){
     super(options);
     this.state = Map({
-      displayed: 'home'
+      page: HOME
     });
 
     this.handlers = {
-      on: NavConstants.PING_STARTING,
-      off: NavConstants.PING_DONE,
-      toggle: NavConstants.POLL_TOGGLED
+      goto: NavConstants.PAGE_REQUESTED
     };
   }
 
   //handlers
 
-  on(){
-    this.replaceState(Map({color: GREEN }));
-  }
-
-  off(){
-    this.replaceState(Map({color: RED }));
-  }
-
-  toggle(){
-    this.state.get('color') === RED ?
-      this.replaceState(Map({ color: GREEN })) :
-    this.replaceState(Map({ color: RED }));
+  // (Page) -> Unit
+  goto(page){
+    this.replaceState(Map({ page: page }));
   }
 
   //accessors
 
-  getColor(){
-    return this.state.get('color');
+  // () -> Page
+  getPage(){
+    return this.state.get('page');
   }
 }
 
-module.exports = GoButtonStore;
+module.exports = NavStore;
