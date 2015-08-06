6033789f758943f2d51743843eb35fa3798139f6
aguestuser <guest.austin@gmail.com>
2015-08-05 22:09:00 -0400

    Map displays multiple pins

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -2,22 +2,23 @@ const Marty = require('marty');
 const BaseComponent = require('./BaseComponent.jsx');
 const { TileLayer, Map, Marker, Popup } = require('react-leaflet');
 const { url, attribution, id, token } = require('../constants/MapSpecs');
-const { s17 } = require('../../test/support/sampleLocations');
+const { s17, nyse2, nyse3 } = require('../../test/support/sampleLocations');
 const pd = require('pretty-date');
 
 class MapContainer extends BaseComponent {
 
     constructor(){
       super();
+      this.bindAll('_markerify', '_positionify');
     }
 
   render(){
 
     return (
-      <div id="map">
+      <div id="map" ref="map">
         <Map center={this._positionify(s17)} zoom={15} >
           <TileLayer url={url} attribution={attribution} accessToken={token} id={id}/>
-          {this._markerify(s17)}
+          {nyse2.map((loc, i) => this._markerify(loc, i))}
         </Map>
       </div>
       );
@@ -29,9 +30,9 @@ class MapContainer extends BaseComponent {
   }
 
   // (Location) -> Marker
-  _markerify(loc){
+  _markerify(loc, i){
     return (
-      <Marker position={this._positionify(loc)} >
+      <Marker position={this._positionify(loc)} key={i} ref={`marker${i}`} >
         <Popup>
           <span>{this._pretty(loc.time)}</span>
         </Popup>
