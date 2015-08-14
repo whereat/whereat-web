48f627228ed4679ea1f075768671375accead0db
aguestuser <guest.austin@gmail.com>
2015-08-14 02:47:24 -0400

    Add specs for `refresh` endpoint * add UserLocationRefresh Record type * clean up mock server config

--- test/modules/api.spec.js
+++ test/modules/api.spec.js
@@ -8,10 +8,11 @@ const config = require('../support/mocks/server/config');
 require('superagent-mock')(http, config);
 
 const api = require('../../src/modules/api');
-const { initRequest, initResponse } = require('../support/sampleLocations');
+const { s17, s17UL, s17_, s17_UL } = require('../support/sampleLocations');
 
-const UserLocation = require('../../src/models/UserLocation');
 const Location = require('../../src/models/Location');
+const UserLocation = require('../../src/models/UserLocation');
+const UserLocationRefresh = require('../../src/models/UserLocationRefresh');
 
 describe.only('Location API', () => {
 
@@ -20,19 +21,43 @@ describe.only('Location API', () => {
     describe('when passed a well-formatted UserLocation', () => {
 
       it('returns a Promise[Array[UserLocation]]', done => {
-        api.init(UserLocation(initRequest))
-          .should.eventually.eql(initResponse)
+        api.init(UserLocation(s17UL))
+          .should.eventually.eql([s17UL])
           .and.notify(done);
       });
     });
 
     describe('when passed an incorrectly formatted UserLocation', () => {
 
-      it('returns a 404 Error', done => {
-        api.init(Location(initRequest))
+      it('returns a Bad Request Error', done => {
+        api.init(Location(s17))
           .should.eventually.eql(new Error(404))
           .and.notify(done);
       });
     });
   });
+
+  describe('#refresh', () => {
+
+    describe('when passed a well-formatted UserLocationRefresh', () => {
+
+      it.only('returns a Promise[Array[UserLocation]]', done => {
+        api.refresh(UserLocationRefresh({ lastPing: 0, location: s17_UL }))
+          .should.eventually.eql([s17UL, s17_UL])
+          .and.notify(done);
+      });
+    });
+
+    describe('when passed an incorrectly formatted UserLocationRefresh', () => {
+
+      it('returns a Bad Request Error', done => {
+        api.init(Location(s17))
+          .should.eventually.eql(new Error(404))
+          .and.notify(done);
+      });
+    });
+  });
+
+
+
 });
