df3132d44e21e4db9c6c863638eb78700eb6fab2
imipolex-g <imipolex-g@riseup.net>
2015-08-14 01:52:22 -0400

    Implement api * Add mock server & fixtures * Test `locations/init` endpoint

--- test/support/sampleLocations.js
+++ test/support/sampleLocations.js
@@ -20,6 +20,7 @@ l.s17UL = {
   time: 1316232000000
 };
 
+
 l.s17_ = {
   lat: 40.7092528,
   lon: -74.0112550,
@@ -156,6 +157,90 @@ l.nyse3ULSeq = Seq.of(
     "lon": -74.010126,
     "time": 2505606400000
   }));
+
+l.initRequest = {
+  id: "75782cd4-1a42-4af1-9130-05c63b2aa9fb",
+  lat: 40.703084,
+  lon: -74.010126,
+  time: 2505606400000
+};
+
+l.initResponse = [{
+  "id": "75782cd4-1a42-4af1-9130-05c63b2aa9fa",
+  "lat": 40.704715,
+  "lon": -74.013685,
+  "time": 2505606400000
+}, {
+  "id": "75782cd4-1a42-4af1-9130-05c63b2aa9fb",
+  "lat": 40.703084,
+  "lon": -74.010126,
+  "time": 2505606400000
+}];
+
+
+l.refreshRequests = [
+  [
+    {
+      lastPing: 0,
+      location: {
+        id: "75782cd4-1a42-4af1-9130-05c63b2aa9fa",
+        lat: 40.705007,
+        lon: -74.012537,
+        time: 2505606400000
+      }
+    },
+    {
+      lastPing: 0,
+      location: {
+        id: "75782cd4-1a42-4af1-9130-05c63b2aa9fb",
+        lat: 40.704092,
+        lon: -74.008989,
+        time: 2505606400000
+      }
+    }
+  ],
+  [
+    {
+      lastPing: 0,
+      location: {
+        id: "75782cd4-1a42-4af1-9130-05c63b2aa9fa",
+        lat: 40.705731,
+        lon: -74.011443,
+        time: 2505606400000
+      }
+    },
+    {
+      lastPing: 0,
+      location: {
+        id: "75782cd4-1a42-4af1-9130-05c63b2aa9fb",
+        lat: 40.705264,
+        lon: -74.010029,
+        time: 2505606400000
+      }
+    }
+  ],
+  [
+    {
+      lastPing: 0,
+      location: {
+        id: "75782cd4-1a42-4af1-9130-05c63b2aa9fa",
+        lat: 40.706886,
+        lon: -74.010853,
+        time: 2505606400000
+      }
+    },
+    {
+      lastPing: 0,
+      location: {
+        id: "75782cd4-1a42-4af1-9130-05c63b2aa9fb",
+        lat: 40.706638,
+        lon: -74.011006,
+        time: 2505606400000
+      }
+    }
+  ]
+];
+
 
 
 module.exports = l;
