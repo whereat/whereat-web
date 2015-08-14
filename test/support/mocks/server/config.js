b8a11e0b383f98f90b432636ebedc2364943cb94
aguestuser <aguestuser@riseup.net>
2015-08-14 02:06:35 -0400

    Refactor mock server * create `correctFormat` helper * consolidate all fixtures to config file

--- test/support/mocks/server/config.js
+++ test/support/mocks/server/config.js
@@ -1,16 +1,13 @@
-const { URL } = require('../../../../src/modules/api');
-const init = require('./initResponse');
-//const refresh = require('./refreshResponse');
+const {  keys, isEqual, isNumber, isString } = require('lodash');
+const { s17UL } = require('../../../support/sampleLocations');
+const { correctFormat } = require('./helpers');
 
-module.exports = [
-  {
+module.exports = [{
   pattern: "https://whereat-server.herokuapp.com/locations/init",
-  fixtures: init,
+  fixtures: (match, data) => correctFormat(data) ? [data] : new Error(404),
   callback: (match, data) => ({ body: data })
-  }
-  // {
-  // pattern: `${URL}/locations/refresh`,
-  // fixtures: refresh,
-  // callback: (match, data) => ({ body: data })
-  // }
-];
+}, {
+  pattern: "https://whereat-server.herokuapp.com/locations/refresh",
+  fixtures: (match, data) => correctFormat(data) ? [s17UL, data] : new Error(404),
+  callback: (match, data) => ({ body: data })
+}];
