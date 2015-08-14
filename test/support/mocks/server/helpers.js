c76f15b87d2fa4847dcdd3e2e27f1b9dc2458183
aguestuser <aguestuser@riseup.net>
2015-08-14 02:47:24 -0400

    Add specs for `refresh` endpoint * add UserLocationRefresh Record type * clean up mock server config

--- test/support/mocks/server/helpers.js
+++ test/support/mocks/server/helpers.js
@@ -2,11 +2,16 @@ const {  keys, isEqual, isNumber, isString } = require('lodash');
 
 const h = {};
 
-h.correctFormat = (data) => (
+h.isUserLocation = (data) =>
   isEqual(keys(data), ['id', 'lat', 'lon', 'time']) &&
-    isString(data.id) === true &&
-    isNumber(data.lat) === true &&
-    isNumber(data.lon) === true &&
-    isNumber(data.time) === true);
+  isString(data.id) === true &&
+  isNumber(data.lat) === true &&
+  isNumber(data.lon) === true &&
+  isNumber(data.time) === true;
+
+h.isUserLocationRefresh = (data) =>
+  isEqual(keys(data), ['lastPing', 'location']) &&
+  isNumber(data.lastPing) &&
+  h.isUserLocation(data.location);
 
 module.exports = h;
