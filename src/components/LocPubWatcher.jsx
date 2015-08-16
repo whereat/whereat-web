f549bba05b612b57da7bcf9a3d84c1a5a89204a2
imipolex-g <imipolex-g@riseup.net>
2015-08-16 16:22:26 -0400

    Implement LocPubWatcher

--- src/components/LocPubWatcher.jsx
+++ src/components/LocPubWatcher.jsx
@@ -1,23 +1,29 @@
 const Marty = require('marty');
 const BaseComponent = require('./BaseComponent.jsx');
 
+//TODO: use pure render mixin so won't re-render when lastPing changes
+
 class LocPubWatcher extends BaseComponent {
   constructor(){
     super();
   }
 
-  componentDidMount(){
-    if (this.props.hasLoc){
+  render(){
+    if(this.props.hasLoc) {
       this.props.firstPing ?
-        this.app.locSubActions.init(this.props.loc)
-        this.app.locSubActions.refresh(this.props.locRefresh)
+        this.app.locSubActions.init(this.props.loc) :
+        this.app.locSubActions.refresh(this.props.locRefresh);
     }
+    return (<div></div>);
   }
 }
 
 module.exports = Marty.createContainer(LocPubWatcher, {
   listenTo: ['locPubStore'],
   fetch: {
-
+    firstPing(){ return this.app.locPubStore.firstPing(); },
+    hasLoc(){ return this.app.locPubStore.hasLoc(); },
+    loc(){ return this.app.locPubStore.getLoc(); },
+    locRefresh() { return this.app.locPubStore.getLocRefresh(); }
   }
 });
