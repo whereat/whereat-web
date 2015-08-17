9f984241d4c525cb210e4cc3fd579349cd35afcd
aguestuser <guest.austin@gmail.com>
2015-08-16 21:21:01 -0400

    Sketch move to api#update for all requests

--- test/support/mocks/server/config.js
+++ test/support/mocks/server/config.js
@@ -5,6 +5,12 @@ const UserLocation = require('../../../../src/models/UserLocation');
 const cb = (match, data) => ({ body: data });
 
 module.exports = [{
+  pattern: "https//whereat-server.herokuapp.com/locations/update",
+  fixtures: (match, data) => isUserLocationRefresh(data) ?
+    [s17UL, UserLocation(data.location.toJS())] :
+    new Error('Bad Request'),
+  callback: cb
+}, {
   pattern: "https://whereat-server.herokuapp.com/locations/init",
   fixtures: (match, data) => isUserLocation(data) ? [data] : new Error('Bad Request'),
   callback: cb
