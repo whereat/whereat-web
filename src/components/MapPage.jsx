4fc28641969dc7fb6d1f067f87302adfdc478884
aguestuser <guest.austin@gmail.com>
2015-08-22 18:25:09 -0400

    Add clear button to map * use glyphicon-remove * calls LocSubAction#remove(User())

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,12 +1,14 @@
 const Marty = require('marty');
 const BaseComponent = require('./BaseComponent.jsx');
 const MapContainer = require('./MapContainer');
+const ClearButton = require('./ClearButton');
 
 class MapPage extends BaseComponent {
   render(){
     return (
       <div className="mapPage" ref="mapPageContents">
         <MapContainer ref="mapContainer"/>
+        <ClearButton ref="clearButton" />
       </div>
     );
   };
