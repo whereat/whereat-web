8402060d008e42e2bc7ee8bee3a1767ce56e5785
aguestuser <guest.austin@gmail.com>
2015-09-03 23:38:28 -0400

    Fix GoButton css for Safari

---
+++ src/modules/system.js
@@ -0,0 +1,11 @@
+const sys = {};
+
+sys.isSafari = () => {
+  return navigator.userAgent.toLowerCase().indexOf('safari') !== -1 &&
+  (
+    navigator.userAgent.toLowerCase().indexOf('chrome') === -1 ||
+    navigator.userAgent.toLowerCase().indexOf('mozilla') === -1
+  );
+};
+
+module.exports = sys;
