dc9c822624d49509fe060a08e50db3c8fd175353
imipolex-g <imipolex-g@riseup.net>
2015-09-03 23:38:28 -0400

    Fix GoButton css for Safari

--- src/components/GoButton.jsx
+++ src/components/GoButton.jsx
@@ -4,7 +4,8 @@ const Tappable = require('react-tappable');
 const { wait } = require('../modules/async');
 const { RED, GREEN } = require('../constants/Colors');
 const { GO_RADIUS, GO_DIAMETER } = require('../constants/Dimensions');
-
+const { isSafari } = require('../modules/system');
+const cn = require('classname');
 
 class GoButton extends BaseComponent {
 
@@ -14,15 +15,35 @@ class GoButton extends BaseComponent {
     this.events = { onClick: this._handleClick, onPress: this._handlePress };
   }
 
+  /* <div className={
+     cn(
+     'funGoButton', {
+     red: this.props.color === RED,
+     green: this.props.color === GREEN
+     })
+     }>
+     </div> */
+
   render(){
     return (
       <Tappable ref="tappable" {...this.events} >
-        <svg ref="svg" className="goButton" width={GO_DIAMETER} height={GO_DIAMETER}>
+        <svg
+         ref="svg"
+         className={ isSafari() ? 'goButtonSafari' : 'goButton' }
+         width={GO_DIAMETER}
+         height={GO_DIAMETER}
+        >
           <circle ref="circle" cx={GO_RADIUS} cy={GO_RADIUS} r={GO_RADIUS} fill={this.props.color} />
         </svg>
+
       </Tappable>
     );
   };
+
+  _goButtonClass(){
+    console.log(isSafari());
+    return isSafari() ? 'goButtonSafari' : 'goButton';
+  }
 
   _handleClick(){
     if (!this.props.polling) {
