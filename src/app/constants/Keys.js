fc1211d9533534ce6dfe57bda21e6a595b6eaf57
The Fisher King <pellam@riseup.net>
2015-09-15 14:40:29 -0400

    use libsodium to generate user ids instead of UUID
    use libsodium.js to generate a random number to serve as the user id, instead of
    node-uuid.

--- src/app/constants/Keys.js
+++ src/app/constants/Keys.js
@@ -1,7 +1,7 @@
-const uuid = require('node-uuid');
-
+// const uuid = require('node-uuid');
+const sodium = require('libsodium-wrappers');
 const keys = {};
 
-keys.USER_ID = uuid.v4();
-
+// keys.USER_ID = uuid.v4();
+keys.USER_ID = sodium.to_hex(sodium.randombytes_buf(sodium.crypto_shorthash_KEYBYTES));
 module.exports = keys;
