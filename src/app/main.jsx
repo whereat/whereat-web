76f6660f28bbb20389a9cce002997225cb5fa351
aguestuser <aguestuser@riseup.net>
2015-09-23 01:32:15 -0400

    Invoke hourly forget from `main.jsx`

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -4,25 +4,25 @@ const Marty = require('marty');
 const Root = require('./components/Root.jsx');
 const React = require('react');
 const Application = require('./application.js');
+const sc = require('./modules/scheduler');
+const hourly = 60 * 60 * 1000;
 require('./styles/main.less');
 
 window.React = React;
 window.Marty = Marty;
 
-if (process.env.NODE_ENV !== 'test'){
+const app = new Application();
+const { ApplicationContainer } = require('marty');
+sc.schedule(app.locSubActions.forget, hourly);
 
-  const app = new Application();
-  const { ApplicationContainer } = require('marty');
+let rootInstance = React.render((
+  <ApplicationContainer app={app}>
+    <Root className="root"/>
+  </ApplicationContainer>
+), document.getElementById('content'));
 
-  let rootInstance = React.render((
-    <ApplicationContainer app={app}>
-      <Root className="root"/>
-    </ApplicationContainer>
-  ), document.getElementById('content'));
-
-  if (module.hot) {
-    require('react-hot-loader/Injection').RootInstanceProvider.injectProvider({
-      getRootInstances: () => [rootInstance]
-    });
-  }
+if (module.hot) {
+  require('react-hot-loader/Injection').RootInstanceProvider.injectProvider({
+    getRootInstances: () => [rootInstance]
+  });
 }
