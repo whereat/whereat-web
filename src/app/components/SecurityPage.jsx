17c8931653509df0e6cb3a48cb9775c68b642667
imipolex-g <imipolex-g@riseup.net>
2015-09-24 13:55:38 -0400

    Solidify SecurityPage
    * revise blurb text
    * add specs
    * add dispatch to `SecurityPage` from `Display` component

--- src/app/components/SecurityPage.jsx
+++ src/app/components/SecurityPage.jsx
@@ -1,21 +1,37 @@
 const Marty = require('marty');
 const BaseComponent = require('./BaseComponent.jsx');
 import { Button } from 'react-bootstrap';
+import { HOME } from '../constants/Pages';
 
 class SecurityPage extends BaseComponent {
+
+  constructor(){
+    super();
+    this.bindAll('_handleClick');
+  }
+
   render(){
     return (
       <div className='securityPage' ref='securityPage'>
-        <h1 className="sec-header" ref="sec-header"> THEY'RE WATCHING YOU!!! </h1>
-        <p className="sec-blurb" ref="sec-blurb">
-          We've done everything we can to ensure your anonimity while using where@. But there are some forms of surveillance we can't protect you from. We HIGHLY RECOMMEND you visit our <a href="https//about.whereat.io/stay-safe">security best practices page</a> to learn what what you can do to stay as safe as possible while using this app! :)
+        <h1 className="secHeader" ref="secHeader"> THEY'RE WATCHING YOU!!! </h1>
+        <p className="secBlurb" ref="secBlurb">
+          While we've done everything we can to ensure your anonimity while using where@, there are some forms of surveillance we can't protect you from. But not to fear! There are things you can do to protect yourself! To learn how, we HIGHLY RECOMMEND you visit our <a href="https//about.whereat.io/stay-safe">security best practices page</a>! :)
         </p>
-        <Button bsStyle="primary" ref='sec-button'>
+        <Button
+          bsStyle="primary"
+          className="secButton"
+          ref="secButton"
+          onClick={this._handleClick}
+          >
           Okay, got it!
         </Button>
       </div>
     );
   };
+
+  _handleClick(){
+    this.app.navActions.goto(HOME);
+  }
 }
 
 module.exports = Marty.createContainer(SecurityPage);
