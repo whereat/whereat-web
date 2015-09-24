eae5574871033c0af26598282397e5461b1b64c2
aguestuser <aguestuser@riseup.net>
2015-09-24 15:40:37 -0400

    Add styling for security alert page

--- src/app/components/SecurityPage.jsx
+++ src/app/components/SecurityPage.jsx
@@ -13,12 +13,18 @@ class SecurityPage extends BaseComponent {
   render(){
     return (
       <div className='securityPage' ref='securityPage'>
-        <h1 className="secHeader" ref="secHeader"> THEY'RE WATCHING YOU!!! </h1>
-        <p className="secBlurb" ref="secBlurb">
-          While we've done everything we can to ensure your anonimity while using where@, there are some forms of surveillance we can't protect you from. But not to fear! There are things you can do to protect yourself! To learn how, we HIGHLY RECOMMEND you visit our <a href="https//about.whereat.io/stay-safe">security best practices page</a>! :)
-        </p>
+        <h1 className="secHeader" ref="secHeader">YOU ARE BEING SPIED ON!!! </h1>
+        <div className="secBlurb" ref="secBlurb">
+          <p>
+            We've done everything we can to ensure your privacy while using where@, but there are some forms of surveillance we can't protect you from. Not to fear: you can protect yourself! To learn how, visit our <a href="https//about.whereat.io/stay-safe">security best practices page.</a>
+          </p>
+          {/* <p className="secSignature">
+              <strong><em>-- {'<3'}, where@</em></strong>
+              </p> */}
+        </div>
         <Button
           bsStyle="primary"
+          bsSize="large"
           className="secButton"
           ref="secButton"
           onClick={this._handleClick}
