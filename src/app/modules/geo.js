be1876aa20a16b793ba85b5635bdffde11ad0e5b
aguestuser <guest.austin@gmail.com>
2015-10-16 12:44:30 -0400

    Refactor location polling
    New way to poll:
    * old implementation of `geo#poll` (using Geololocation.watchPosition)
      didn't allow for getting updates at set intervals
    * replace with scheduled calls to `geo#getCurrentPosition`(via
      `scheduler#schedule)
    
    New UI for go button
    * remove UI logic for "long-pressing" GoButton
    * instead, clicking turns on polling, no logic for a single ping
    
    Decrease notification interval

--- src/app/modules/geo.js
+++ src/app/modules/geo.js
@@ -9,15 +9,4 @@ geo.get = (nav = navigator) => (
       reject('Phone not providing location.') :
       nav.geolocation.getCurrentPosition(p => resolve(p)))));
 
-// ((LatLon => Unit), (Error => Unit), Number) -> Number [id]
-geo.poll = (publisher, err, sec = USER_LOCATION_INTERVAL) => (
-  navigator.geolocation.watchPosition(
-    publisher, err, {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: sec * 1000 }));
-
-// (Number) -> Unit
-geo.stopPolling = (id) => navigator.geolocation.clearWatch(id);
-
 module.exports = geo;
