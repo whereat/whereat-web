392bb410a08d6633ac19b90cfa1f1389f4b0e1bf
imipolex-g <imipolex-g@riseup.net>
2015-10-17 16:06:36 -0400

    Add `SettingsStore` with `#setShare`, `#getShare`

---
+++ src/app/stores/SettingsStore.js
@@ -0,0 +1,34 @@
+import Marty from 'marty';
+import { Map } from 'immutable';
+import SettingsConstants from '../constants/SettingsConstants';
+import Settings from '../constants/Settings';
+//const { share } = Settings;
+
+class SettingsStore extends Marty.Store {
+
+  constructor(options){
+    super(options);
+    this.state = Map({
+      share: 2
+    });
+
+    this.handlers = {
+      setShare: SettingsConstants.SHARE_INTERVAL_CHANGED
+    };
+  }
+
+  //handlers
+
+  //(Number) -> Unit
+  setShare(index){
+    this.replaceState(this.state.set('share', index));
+  }
+
+  //accessors
+
+  getShare(){
+    return this.state.get('share');
+  }
+}
+
+export default SettingsStore;
---
+++ src/test/stores/SettingsStore.spec.js
@@ -0,0 +1,80 @@
+import sinon from'sinon';
+import chai from'chai';
+import sinonChai from'sinon-chai';
+
+const should = chai.should();
+chai.use(sinonChai);
+
+import Marty from'marty';
+import Application from'../../app/application';
+
+import { Map } from 'immutable';
+import { dispatch, hasDispatched, createApplication } from 'marty/test-utils';
+import {
+  shouldHaveDispatched,
+  shouldHaveDispatchedWith,
+  shouldHaveDispatchedWithImmutable,
+  shouldHaveBeenCalledWithImmutable,
+  shouldHaveObjectEquality
+} from '../support/matchers';
+
+import SettingsConstants from '../../app/constants/SettingsConstants';
+
+describe('Settings Store', () => {
+
+  const s1 = Map({share: 1});
+  const s2 = Map({share: 2});
+
+  const setup = (state) => {
+    const app = createApplication(Application, {include: ['settingsStore'] });
+    app.settingsStore.state = state;
+    const listener = sinon.spy();
+    app.settingsStore.addChangeListener(listener);
+    return [app, listener];
+  };
+
+  describe('handlers', () => {
+
+    describe('#setShare', () => {
+
+      it('handles SHARE_INTERVAL_CHANGED', () =>{
+        const app = setup(s1);
+        const setShare = sinon.spy(app.settingsStore, 'setShare');
+        dispatch(app, SettingsConstants.SHARE_INTERVAL_CHANGED, 2);
+
+        setShare.should.have.been.calledWith(2);
+
+        setShare.restore();
+      });
+
+      it('updates the `share` field', () => {
+        const app = setup(s1);
+        app.settingsStore.setShare(2);
+
+        shouldHaveObjectEquality(app.settingsStore.state, s2);
+      });
+
+      it('notifies listeners of state change', () =>{
+        const [app, listener] = setup(s1);
+        app.settingsStore.setShare(2);
+
+        shouldHaveBeenCalledWithImmutable(listener, s2);
+
+      });
+    });
+  });
+
+  describe('accessors', () => {
+
+    describe('#getShare', () => {
+
+      it('gets value of `share` field', () => {
+        const [app] = setup(s1);
+        app.settingsStore.getShare().should.equal(1);
+
+        const [app2] = setup(s2);
+        app2.settingsStore.getShare().should.equal(2);
+      });
+    });
+  });
+});
