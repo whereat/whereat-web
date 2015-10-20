27dbcd359518dd3973aad24ed99ddbd0b5b9f5e6
imipolex-g <imipolex-g@riseup.net>
2015-10-20 14:07:00 -0400

    Changes to share settings trigger poll reset
    * add `isPolling` and `pollId` prop to `SettingsPage`
    * call `locPubActions.poll/stopPolling` if `isPolling`

--- src/test/components/SettingsPage.spec.js
+++ src/test/components/SettingsPage.spec.js
@@ -8,10 +8,17 @@ const Application = require('../../app/application');
 const { createStore, createApplication } = require('marty/test-utils');
 const testTree = require('react-test-tree');
 import { Map } from 'immutable';
+import { merge } from 'lodash';
 
 import {
   shouldHaveBeenCalledWith
 } from '../support/matchers';
+import {
+  emptyState,
+  ping1State,
+  ping2State,
+  pollState
+} from '../support/sampleLocPubStates';
 import { s1, s2 } from '../support/sampleSettings';
 
 
@@ -22,34 +29,48 @@ const { share } = Settings;
 
 describe('SettingsPage component', () => {
 
-  const setup = (state) => {
+  const setup = (stgState = s1, locPubState = emptyState) => {
 
-    const spies = {
+    const stgSpies = {
       setShare: sinon.spy(),
       setTtl: sinon.spy()
     };
 
-    const app = createApplication(Application, {
-      include: ['settingsStore'],
-      stub: { settingsActions: spies }
-    });
-    app.settingsStore.state = state;
+    const locPubSpies = {
+      poll: sinon.spy(),
+      stopPolling: sinon.spy()
+    };
+    const spies = merge({}, stgSpies, locPubSpies);
 
-    const component = propTree(app, state.get('share'));
+    const app = createApplication(Application, {
+      include: ['settingsStore', 'locPubStore'],
+      stub: {
+        settingsActions: stgSpies,
+        locPubActions: locPubSpies
+      }
+    });
+    app.settingsStore.state = stgState;
+    app.locPubStore.state = locPubState;
+
+    const component = propTree(
+      app,
+      stgState.get('share'),
+      locPubState.get('isPolling'),
+      locPubState.get('pollId')
+    );
+
     return [app, component, spies];
   };
 
-  const propTree = (app, share) =>(
-    testTree(<SettingsPage.InnerComponent curShare={share} />, specs(app)));
+  const propTree = (app, share, isPolling, pollId) =>
+    testTree(<SettingsPage.InnerComponent
+               curShare={share}
+               isPolling={isPolling}
+               pollId={pollId}
+             />, specs(app));
 
   const tree = (app) => testTree(<SettingsPage />, specs(app));
-
-  const specs = (app) => ({
-    context: { app: app }
-  });
-
-  const stateTree = (state) => testTree(<SettingsPage.InnerComponent />, stateOf(state));
-  const stateOf = (state) => ({context: {state: state}});
+  const specs = (app) => ({context: { app: app }});
 
   describe('contents', () => {
 
@@ -92,20 +113,66 @@ describe('SettingsPage component', () => {
         comp2.innerComponent.getProp('curShare').should.equal(2);
       });
     });
+
+    describe('isPolling prop', () => {
+
+      it('reacts to LocPubStore#getIsPolling()', () =>{
+        const [app] = setup(s1, emptyState);
+        const comp = tree(app);
+        comp.innerComponent.getProp('isPolling').should.be.false;
+
+        app.locPubStore.pollingOn(1);
+        const comp2 = tree(app);
+        comp2.innerComponent.getProp('isPolling').should.be.true;
+      });
+
+      it('reacts to LocPubStore#getPollId', () => {
+        const [app] = setup(s1, emptyState);
+        const comp = tree(app);
+        comp.innerComponent.getProp('pollId').should.equal(-1);
+
+        app.locPubStore.pollingOn(1);
+        const comp2 = tree(app);
+        comp2.innerComponent.getProp('pollId').should.equal(1);
+      });
+    });
   });
 
   describe('interactivity', () => {
 
     describe('#_handleShareSelect', () => {
 
-      it('calls settingsActions#setShare', () => {
-        const [app, comp, {setShare}] = setup(s1);
+      describe('when not polling', () => {
 
-        comp.shareItems2.simulate.select();
-        setShare.should.have.been.calledWith(2);
+        it('calls settingsActions#setShare but not reset polling', () => {
+          const [app, _, {poll, stopPolling, setShare}] = setup(s1);
+          const comp = tree(app).innerComponent;
 
-        comp.shareItems3.simulate.select();
-        setShare.should.have.been.calledWith(3);
+          comp.shareItems2.simulate.select();
+          setShare.should.have.been.calledWith(2);
+
+          comp.shareItems3.simulate.select();
+          setShare.should.have.been.calledWith(3);
+
+          stopPolling.should.not.have.beenCalled;
+          poll.should.not.have.beenCalled;
+        });
+
+        it('calls settingsActions#setShare and resets polling', () => {
+          const [app, _, {poll, stopPolling, setShare}] = setup(s1, pollState);
+          const comp = tree(app).innerComponent;
+          comp.getProp('isPolling').should.beTrue;
+
+          comp.shareItems2.simulate.select();
+
+          setShare.should.have.been.calledWith(2);
+          stopPolling.should.have.been.calledWith(1);
+          poll.should.have.been.calledWith(share[2]);
+
+          comp.shareItems3.simulate.select();
+          stopPolling.should.have.been.calledWith(1);
+          poll.should.have.been.calledWith(share[3]);
+        });
       });
     });
   });
