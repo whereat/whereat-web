afd2c36589187018f3849f6b07c68e115eeb63cd
imipolex-g <imipolex-g@riseup.net>
2015-10-21 15:07:34 -0400

    Add `SettingsStore#setLocTtl` / `#getLocTtl`

--- src/test/support/sampleSettings.js
+++ src/test/support/sampleSettings.js
@@ -3,8 +3,8 @@ import { Map } from 'immutable';
 export default {
   s1: Map({shareFreq: 1}),
   s2: Map({shareFreq: 2}),
-  s1t1: Map({shareFreq: 1, ttl: 1}),
-  s1t2: Map({shareFreq: 1, ttl: 2}),
-  s2t1: Map({shareFreq: 2, ttl: 1}),
-  s2t2: Map({shareFreq: 2, ttl: 2})
+  s1t1: Map({shareFreq: 1, locTtl: 1}),
+  s1t2: Map({shareFreq: 1, locTtl: 2}),
+  s2t1: Map({shareFreq: 2, locTtl: 1}),
+  s2t2: Map({shareFreq: 2, locTtl: 2})
 };
