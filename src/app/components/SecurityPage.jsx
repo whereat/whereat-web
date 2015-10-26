ff074ace25af9e1f3f1674020f442a4fdbb37189
aguestuser <aguestuser@riseup.net>
2015-10-26 15:05:52 -0400

    Add two-button layout to `SecurityAlertPage`
    * remove link from blurb
    * add yes/no buttons with green/red backgrounds
    * clicking yes button links to about.whereat.io/stay-safe

--- src/app/components/SecurityPage.jsx
+++ src/app/components/SecurityPage.jsx
@@ -13,21 +13,40 @@ class SecurityPage extends BaseComponent {
   render(){
     return (
       <div className='securityPage' ref='securityPage'>
-        <h1 className="secHeader" ref="secHeader">YOU ARE BEING SPIED ON!!! </h1>
+
+        <h1 className="secHeader" ref="secHeader">YOU'RE BEING SPIED ON!!! </h1>
+
         <div className="secBlurb" ref="secBlurb">
           <p>
-            We've done everything we can to ensure your privacy while using where@, but there are some forms of surveillance we can't protect you from. Not to fear: you can protect yourself! To learn how, visit our <a ref="staySafeLink" href="https://about.whereat.io/stay-safe">security best practices page.</a>
+            We've done everything we can to ensure your privacy while using where@, but there are some forms of surveillance we can't protect you from. Not to fear: you can protect yourself! To learn how, visit our security best practices page.
           </p>
         </div>
-        <Button
-          bsStyle="primary"
-          bsSize="large"
-          className="secButton"
-          ref="secButton"
-          onClick={this._handleClick}
-          >
-          Okay, got it!
-        </Button>
+
+        <div ref="secButtons" className="secButtons" >
+
+          <Button
+            bsStyle="primary"
+            bsSize="large"
+            className="secButton secNoButton"
+            ref="secNoButton"
+            onClick={this._handleClick}
+            >
+            Stay exposed
+          </Button>
+
+          <Button
+            bsStyle="primary"
+            bsSize="large"
+            className="secButton secYesButton"
+            ref="secYesButton"
+            >
+            <a ref="staySafeLink" href="https://about.whereat.io/stay-safe">
+              Get protection
+            </a>
+          </Button>
+
+        </div>
+
       </div>
     );
   };
